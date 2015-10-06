9bdde3f7987c3feda0a9b44efab449464ae6c44a
Madelyn Kasula <madelynkasula@gmail.com>
2015-10-05 20:31:26 -0500

    Use ffaker to seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'ffaker'
+
+10.times do
+  User.create!(email: FFaker::Internet.email, password: "password")
+end
+
+20.times do
+  Wiki.create!(description: FFaker::LoremFR.phrase)
+end
+
+50.times do
+  Article.create!(title: FFaker::Movie.title, content: FFaker::LoremFR.paragraph, published: true, needs_sources: false, wiki_id: (1..20).to_a.sample)
+end
+
+50.times do
+  Article.create!(title: FFaker::Movie.title, content: FFaker::LoremFR.paragraph, published: false, needs_sources: true, wiki_id: (1..20).to_a.sample)
+end
+
+50.times do
+  Category.create!(name: FFaker::InternetSE.company_name_single_word)
+end
+
+users = User.all
+users.each do |user|
+  5.times do
+    user.wikis << Wiki.all.sample
+  end
+end
+
+articles = Article.all
+articles.each do |article|
+  3.times do
+    article.categories << Category.all.sample
+  end
+end
