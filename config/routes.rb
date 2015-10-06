5599f0f12f71bf3085696db22c6caa15138d3897
David Campbell <davidcampbell.itprofessional@gmail.com>
2015-10-05 20:46:33 -0500

    Add bootstrap components and partial layout.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  root 'wiki#index'
+
+  resources :wiki, except: [:edit, :update] do
+    resources :articles
+  end
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
