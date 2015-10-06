e2aa47c232ec0dc08ebb3fa8cdd4e523336ceac8
Madelyn Kasula <madelynkasula@gmail.com>
2015-10-06 12:48:19 -0500

    Fix all links in navbar to redirect using AngularJS

--- app/assets/javascripts/ng-app/components/app-controller.js
+++ app/assets/javascripts/ng-app/components/app-controller.js
@@ -1,4 +1,9 @@
 angular.module('AngularRails')
     .controller('HomeCtrl', function ($scope) {
         $scope.things = ['Angular', 'Rails 4.1', 'Working', 'Together!!'];
-    });
+    })
+    .controller('UserCtrl', ['$scope', '$window', function ($scope, $window) {
+      $scope.go = function(hash) {
+        $window.location.href = hash;
+      };
+    }]);
