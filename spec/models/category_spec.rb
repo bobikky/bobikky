5ecbf1824c9a74ca518f60c145373e7ee0d73ae6
[01;31m[K"name"[m[K: "Wyeth Jackson <[01;31m[K"email"[m[K: "wyethjackson@gmail.com>
2015-10-06 13:38:42 -0500

    association rspec tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Category do 
+	describe 'associations' do
+		before(:each) do
+			@wiki = Wiki.create!(description: "TV show wiki")
+			@category = Category.create!(name: "TV Show")
+			@article1 = Article.create!(title: "Narcos", content: "good show", published: true, needs_sources: false, wiki_id: @wiki.id)
+			@article2 = Article.create!(title: "Game of Thrones", content: "good show", published: true, needs_sources: false, wiki_id: @wiki.id)
+			@article3 = Article.create!(title: "Modern Family", content: "good show", published: true, needs_sources: false, wiki_id: @wiki.id)
+			
+			ArticleCategory.create!(article_id: @article1.id, category_id: @category.id)
+			ArticleCategory.create!(article_id: @article2.id, category_id: @category.id)
+			ArticleCategory.create!(article_id: @article3.id, category_id: @category.id)
+		end
+
+		it 'returns the articles in a specific category' do
+			expect(@category.articles).to eq([@article1, @article2, @article3])
+		end
+	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do 
+	describe 'associations' do
+		before(:each) do
+			@user = User.create!(email: "wyethjackson@gmail.com", password: "123")
+			@wiki1 = Wiki.create!(description: "TV Shows")
+			@wiki2 = Wiki.create!(description: "TV Shows")
+			@wiki3 = Wiki.create!(description: "TV Shows")
+			
+			UserWiki.create!(user_id: @user.id, wiki_id: @wiki1.id)
+			UserWiki.create!(user_id: @user.id, wiki_id: @wiki2.id)
+			UserWiki.create!(user_id: @user.id, wiki_id: @wiki3.id)
+		end
+
+		it 'returns the articles in a specific category' do
+			expect(@user.wikis).to eq([@wiki1, @wiki2, @wiki3])
+		end
+	end
 end
