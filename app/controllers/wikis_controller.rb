79e80d3c23b6a60c946e01328052078064c8d763
David Campbell <davidcampbell.itprofessional@gmail.com>
2015-10-06 14:42:43 -0500

    Finish index view and controller

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -3,11 +3,23 @@ class WikisController < ApplicationController
 	# before_filter :authenticate_user!, :except => [:index]
 
   def index
+    @final_array = []
     @wikis = Wiki.all
+    @wikis.each do |wiki|
+			articles = Article.where(wiki_id: wiki.id).first(2)
+			@final_array << wiki.as_json.merge({"articles" =>articles})
+    end
   	respond_to do |format|
   		format.html
-  		format.json { render json: @wikis }
+  		format.json { render json: @final_array }
   	end
+  end
+
+  def show
+  	@wiki = Wiki.find_by(id: params[:id])
+  	@articles = Articles.where(wiki_id: params[:id])
+
+  	
   end
 
 end
