9c40d2c549fc8f4308ededcc9b13f0a5d7424e49
Madelyn Kasula <madelynkasula@gmail.com>
2015-10-05 19:54:21 -0500

    Devise sometimes works - very buggy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :recoverable
+  devise :database_authenticatable, :registerable
 
   has_many :user_wikis
   has_many :wikis, { through: :user_wikis }
