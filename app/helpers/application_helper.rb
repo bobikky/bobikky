f8d7f861058b8f03ac2477fa65ac93f137fd6602
Madelyn Kasula <madelynkasula@gmail.com>
2015-10-07 15:14:32 -0500

    Add admin capability to wiki_member? helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 
 	def wiki_member?(userID, wikiID)
+		return true if User.find(userID).admin?
 		test = UserWiki.where("user_id=? and wiki_id=?", userID, wikiID)
 		test.length > 0
 	end
