278271136cd2b8fb61ace20df22a33e7630349d3
Madelyn Kasula <madelynkasula@gmail.com>
2015-10-06 20:14:39 -0500

    Most of new article form complete - need to implement article#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
+
   root 'wikis#index'
-   # get '*path' => 'wiki#index'
+
   resources :wikis, except: [:edit, :update] do
     resources :articles
   end
